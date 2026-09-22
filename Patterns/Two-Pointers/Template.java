4e2c3c642d48c340c80050a45068b08a783e942b
Mahesh <283988710+maheshofficial0011@users.noreply.github.com>
2026-09-23 01:25:59 +0530

    feat: add two pointers template

---
+++ Patterns/Two-Pointers/Template.java
@@ -0,0 +1,14 @@
+public class TwoPointersTemplate {
+    public static void reverse(int[] numbers) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int temp = numbers[left];
+            numbers[left] = numbers[right];
+            numbers[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
